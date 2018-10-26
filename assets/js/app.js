c0f0d39c43f107e38bd1bacfc5ba1733ee4aa0bd
yogita <veryogita@gmail.com>
2018-10-26 17:43:15 +0530

    files updated new navbar created

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+/*  $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-toggle:visible').click();
+});
+//take care of sub-menu
+$(function () { 
+  $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () { 
+          $('.navbar-toggle:visible').click(); 
+  }); 
+});
+$(function () { $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () { $('.navbar-toggle:visible').click(); }); });
+//support for phone touch.
+$(function () {
+  $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+          $('.navbar-toggle:visible').click();
+  });
+});
+*/
+$('.navbar-collapse ul li a').click(function(){ 
+  $('.navbar-toggle:visible').click();
+  });
+  
+  
+});// closed document.ready
+
+       
